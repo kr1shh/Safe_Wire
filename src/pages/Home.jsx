ddafcdfa22dfadf321c86ad9ced95a89de025661
Binnukrishna <binnukrishna5233@gmail.com>
2024-09-04 19:53:23 +0530

    home page sample done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,27 @@
+import { useNavigate } from "react-router-dom"
 import "./home.scss"
+import heroBg from "../assets/ContourLine.svg"
 
 const Home = () => {
+  const navigate = useNavigate()
   return (
-    <div>Home</div>
+    <>
+      <main>
+        <div className="hero-container">
+          <div className="hero-content">
+            <h1>Safe Wire</h1>
+            <p>
+            Every transaction matters secure them with our advanced fraud detection. Prevent threats before they strike, keeping your finances safe and your business thriving. Trust us to protect what’s important. 
+            </p>
+            <div className="hero-btns">
+              <button onClick={ ()=>navigate("/login") }>Login</button>
+              <button onClick={ ()=>navigate("/register") }>Sign Up</button>
+            </div>
+          <img src={ heroBg } alt="hero background" />
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
 
